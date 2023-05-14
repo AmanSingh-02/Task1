f24879aeace387d14e7cc634fbca4018e01c3a3c
AmanSingh-02 <133587871+AmanSingh-02@users.noreply.github.com>
2023-05-14 20:51:54 +0530

    Add files via upload

---
+++ task1.css
@@ -0,0 +1,46 @@
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Ubuntu:wght@300&display=swap');
+html, body {
+  height: 100%;
+  width: 100%;
+  font-family: 'Roboto', sans-serif; 
+}
+
+.button{
+  width: 69px;
+  padding: 15px;
+  margin: 0 5px;
+  border: 2px solid black;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.row{
+  margin: 8px 0;
+}
+.row input{
+  width: 291px;
+  font-size: 20px;
+    margin: 0;
+    padding: 10px 0px;
+    border: 2px solid black;
+    border-radius: 5px;
+}
+.container{
+
+  padding: 20px;
+
+}
+
+.bucket{
+  background-color: rgb(113, 225, 117);
+  border-style:solid;
+  border-radius: 5px;
+
+}
+body{
+  background-color: rgb(241, 145, 21);
+        background-size: cover;
+}
+h3{
+  text-align: center;
+}
